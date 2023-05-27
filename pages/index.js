3ed521300c491e9ffe95beb65eecdc3afcc673ac
schwarzsky <bkoray@protonmail.com>
2023-05-27 18:07:11 +0300

    fix: design

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
-import {User, UserStatistic} from "../lib/db";
+import {StaffTag, User, UserStatistic} from "../lib/db";
 import {useEffect, useState} from "react";
 
-export default function Home({users}) {
+export default function Home({users, usersCount, userStatisticsCount, staffTagsCount}) {
   const [stats, setStats] = useState(JSON.parse(users))
 
   return (
     <div className="container mx-auto min-h-screen py-6">
       <h3 className="text-3xl font-semibold">SonOyuncu eski sıralama - ilk 300</h3>
+      <ul className="flex gap-6 font-mono text-sm font-semibold text-gray-400 list-disc">
+        <li>Toplam kayıtlı kullanıcı: {usersCount}</li>
+        <li>Kayıtlı kullanıcı istatiği {userStatisticsCount}</li>
+        <li>Yetkili etiketleme istatistiği: {staffTagsCount}</li>
+      </ul>
       <ul className="mt-4 flex flex-col gap-2">
         {stats.map((u, i) => {
           return (
             <li key={u.userId}
-                className="border border-gray-300 rounded-md px-4 py-2 flex justify-between items-center">
+                className="border border-gray-300 rounded-md px-4 py-2 flex justify-between items-center hover:font-semibold hover:border-orange-400">
               <p className="flex-[1]">
                 <strong className="text-lg text-gray-400">#{i+1}</strong> {u.username}
               </p>
@@ -32,6 +37,9 @@ export async function getServerSideProps() {
   await mongoose.connect(process.env.MONGO_CONNECT)
 
   const userStatistics = await UserStatistic.find().limit(300).sort({total: 'desc'})
+  const usersCount = await User.count({})
+  const userStatisticsCount = await UserStatistic.count({})
+  const staffTagsCount = await StaffTag.count({})
   const userWithStatistics = []
 
   await Promise.all(
@@ -51,7 +59,10 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      users: JSON.stringify(userWithStatistics)
+      users: JSON.stringify(userWithStatistics),
+      usersCount,
+      userStatisticsCount,
+      staffTagsCount
     }
   }
 }
