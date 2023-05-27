216dd693af8814d9fb22fd7a95733b287acefa59
schwarzsky <bkoray@protonmail.com>
2023-05-27 04:50:55 +0300

    new: project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function SonOyuncuEskiSiralama({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default SonOyuncuEskiSiralama
